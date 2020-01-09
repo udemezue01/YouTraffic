20dcf6f5a8467d51387654c6833fc8a966f51981
udemezue John <johnzeus14@gmail.com>
2020-01-09 03:05:25 +0100

    bootstrapped projects

--- post/models.py
+++ post/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Hasghtag(models.Model):
+	title 			 = models.CharField()
+
+
+class Post(models.Model):
+
+	User 			 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+
+	text 			 = models.CharField()
+	image 			 = models.FileField()
+	video 			 = models.FileField()
+
+	hashtag 		 = models.ManyToManyField(Hashtag)
+
+	created_at 		 = models.DateTimeField(auto_now_add = True)
+
---
+++ youtraffic/settings/__init__.py
@@ -0,0 +1,11 @@
+
+from .prod import *
+from .base import *
+
+
+try:
+	from .local import *
+   
+except:
+	pass
+   
